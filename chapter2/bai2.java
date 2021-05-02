e55b299799bd79d851b98c157781f1e3f7a53dc2
LyTranHoangHieu <73414103+LyTranHoangHieu@users.noreply.github.com>
2021-05-02 10:12:58 +0700

    Add files via upload

---
+++ chapter2/bai2.java
@@ -0,0 +1,13 @@
+package chapter2;
+
+public class bai2 {
+    public static void main(String[] args) {
+    int a = 3, b = 0;
+    System.out.println("Tong a + b = " + (a + b));
+    System.out.println("Hieu a - b = " + (a - b));
+    if(b != 0)
+        System.out.println("Thuong a / b = " + (a / b));
+    else
+        System.out.println("Khong chia duoc");
+    }
+}
